98bfb38fdbad44e92687b6e0ad8d77c8f4847afd
Artyom <sgnoteii55@gmail.com>
2024-05-10 00:07:32 +0400

    chore: BooksService updated

--- main/java/org/borodin/library/services/BooksService.java
+++ main/java/org/borodin/library/services/BooksService.java
@@ -4,7 +4,6 @@ import org.borodin.library.models.Book;
 import org.borodin.library.repositories.BooksRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
